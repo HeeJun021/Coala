a7ddf71e3afa958a4ab016d4ef96d151759a12c8
HeeJun021 <amlwns21119@gmail.com>
2025-02-02 16:28:50 +0900

    250202 colors
    자주 쓰이는 색들 선언해뒀음
    navbar 진한 초록색
    accent 밝은 초록색
    dark 어두운 회색
    white 흰색
    popup 팝업버튼 색

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"], // src 폴더 내 모든 js, jsx, ts, tsx 파일에서 TailwindCSS 사용 가능
   theme: {
-    extend: {}, // 사용자 정의 테마 확장
+    extend: {
+      colors: {
+        navbar: "#81A978", //상단바 기본 색깔
+        accent: "#A7DA9B", //버튼 등 약간 밝은 초록색
+        dark: "#959595", //하이라이트 죽은 색
+        white: "#FFFFFF", //흰 색
+        popup: "#F3EDCD" //팝업버튼이나 마이페이지에 둘 색
+      }
+    }, // 사용자 정의 테마 확장
   },
   plugins: [],
 }
